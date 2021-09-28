e897eac63b0fbbfbb49c58d0a7a2f6fbdd32fe24
haugaard1101 <s205434@student.dtu.dk>
2021-09-28 09:21:55 +0200

    Dice

--- 07_del1/src/spil/Dice.java
+++ 07_del1/src/spil/Dice.java
@@ -2,7 +2,42 @@ package spil;
 
 public class Dice {
 
-    public static void main(String[] args) {
-        // write your code here
+    private final int MAX = 6; //maksimalt antal øjne
+
+    private int faceValue; //nuværende antal øjne
+    private int faceValue2;
+
+    public Dice() //Constructor: sætter det begyndende antal øjne
+    {
+        faceValue = 1;
+        faceValue2 = 1;
     }
+
+    public int roll() //kaster terningen og retunerer resultatet
+    {
+      faceValue = (int)(Math.random() * MAX) + 1;
+        faceValue2 = (int)(Math.random() * MAX) + 1;
+
+      return faceValue + faceValue2;
+
+    }
+
+public void setFaceValue (int value) //Mutator
+{
+    faceValue = value;
+    faceValue2 = value;
+}
+
+public int getFaceValue() //Accessor
+{
+    return faceValue + faceValue2;
+}
+
+public  String toString() //retunerer en String repræsentation af terningen
+{
+    String result = Integer.toString(faceValue + faceValue2);
+
+    return result;
+}
+
 } 
